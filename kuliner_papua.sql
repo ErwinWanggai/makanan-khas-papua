25b38271ed60dec3c77e9725d35821103f0bd534
Anisha Aryanti <anishadnna@gmail.com>
2025-05-27 14:21:31 +0900

    database baru

--- kuliner_papua.sql
+++ kuliner_papua.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: May 27, 2025 at 04:53 AM
+-- Generation Time: May 27, 2025 at 05:20 AM
 -- Server version: 8.0.30
 -- PHP Version: 8.3.13
 
@@ -20,6 +20,27 @@ SET time_zone = "+00:00";
 --
 -- Database: `kuliner_papua`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `contacts`
+--
+
+CREATE TABLE `contacts` (
+  `id` int NOT NULL,
+  `full_name` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `phone` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `message` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `contacts`
+--
+
+INSERT INTO `contacts` (`id`, `full_name`, `email`, `phone`, `message`) VALUES
+(2, 'Joy Kennedy', 'livew@mailinator.com', '+1 (925) 683-7514', 'Quia ut enim est ve');
 
 -- --------------------------------------------------------
 
@@ -43,9 +64,34 @@ INSERT INTO `foods` (`id`, `name`, `description`, `image`) VALUES
 (2, 'Tad Hatfield', 'Anim cumque illum a', 'uploads/683542bbefbf2_wallpaperflare.com_wallpaper (1).jpg'),
 (3, 'Gary Macias', 'Do facilis est quis', 'uploads/683544789b480_ustj.jpg');
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `password`) VALUES
+(1, 'admin', '$2y$10$S0SEqaj31rqlNz6rq5/rqupP.SlQEsnhlt.Vhcodv6.TTyGAiO0Xm');
+
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `contacts`
+--
+ALTER TABLE `contacts`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `foods`
@@ -54,14 +100,32 @@ ALTER TABLE `foods`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
+
+--
+-- AUTO_INCREMENT for table `contacts`
+--
+ALTER TABLE `contacts`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `foods`
 --
 ALTER TABLE `foods`
   MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
